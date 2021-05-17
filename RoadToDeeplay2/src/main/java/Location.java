02320eb655b084ce443d548b534ccd31d2f9013f
Neodmit <52948791+Neodmit@users.noreply.github.com>
2021-05-18 01:50:30 +0600

    Add files via upload

---
+++ RoadToDeeplay2/src/main/java/Location.java
@@ -0,0 +1,16 @@
+public class Location {
+    int col, row;
+
+    public Location(int row, int col) {
+        this.row = row;
+        this.col = col;
+    }
+
+    public int getCol() {
+        return col;
+    }
+
+    public int getRow() {
+        return row;
+    }
+}
